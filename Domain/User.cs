37ce47392d108ff5525628c57fe20293afb2c709
Danil <mastermcpe691@gmail.com>
2026-04-19 12:13:10 +0600

    Added soft delete field

--- Domain/User.cs
+++ Domain/User.cs
@@ -3,5 +3,6 @@
     public int Id { get; set; }
     public string Name { get; set; }
     public string PasswordHash { get; set; }
+    public bool IsDeleted { get; set; }
     public List<ChatParticipant> Chats { get; set; }
 }
